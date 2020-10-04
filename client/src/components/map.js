c31c7590c2545337460a118e0a26ed803cb2cb61
Nimish Shah <nimishshah2000@gmail.com>
2020-10-04 23:10:42 +0530

    many api checks, mapbox static image

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -5,6 +5,7 @@ import "./Map.css";
 var date = new Date().toISOString().slice(0, 10);
 var tilePathPopulation = `wmts/epsg3857/best/GPW_Population_Density_2020/default/${date}/GoogleMapsCompatible_Level7/{z}/{y}/{x}.png`;
 var tilePathGeneral = `wmts/epsg3857/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/${date}/GoogleMapsCompatible_Level9/{z}/{y}/{x}.jpg`;
+
 mapboxgl.accessToken =
   "pk.eyJ1IjoidGVhbWFudHJpa3NoIiwiYSI6ImNrZnQ3dHUxNjBwMjQycXBkNTB6Y2NidXYifQ.bJUa4zjIUDITAZLgc8hz2A";
 
@@ -14,7 +15,7 @@ const Map = () => {
   const [lng, setLng] = useState(5);
   const [lat, setLat] = useState(34);
   const [zoom, setZoom] = useState(1.5);
-
+  const [imgsrc, setImgsrc] = useState("");
   // Initialize map when component mounts
   useEffect(() => {
     map = new mapboxgl.Map({
@@ -43,18 +44,20 @@ const Map = () => {
         },
         layers: [
           {
-            id: "population",
-            type: "raster",
-            source: "population",
-            minzoom: 0,
-            maxzoom: 10,
-          },
-          {
             id: "general",
             type: "raster",
             source: "general",
             minzoom: 0,
             maxzoom: 10,
+            layout: { visibility: "visible" },
+          },
+          {
+            id: "population",
+            type: "raster",
+            source: "population",
+            minzoom: 0,
+            maxzoom: 10,
+            layout: { visibility: "none" },
           },
         ],
       },
@@ -71,25 +74,42 @@ const Map = () => {
       setZoom(map.getZoom().toFixed(2));
     });
 
+    var marker = [
+      new mapboxgl.Marker().setLngLat([12.550343, 55.665957]),
+      new mapboxgl.Marker().setLngLat([12.550343, 65.665957]),
+    ];
+    marker.forEach((mark) => mark.addTo(map));
     // Clean up on unmount
+
     return () => map.remove();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const getPic = () => {
-    console.log(map.getCanvas().toDataURL("images/png"));
-    // window.open(map.getCanvas().toDataURL("image/jpeg"));
+    // var linkSource = map.getCanvas().toDataURL();
+    // // setImgsrc(map.getCanvas().toDataURL("images/png"));
+    // // window.open(map.getCanvas().toDataURL("image/jpeg"));
+    // const downloadLink = document.createElement("a");
+    // downloadLink.href = linkSource;
+    // downloadLink.download = "a.png";
+    // downloadLink.click();
+    var a =
+      "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/-77.03968,38.89744,5,0/300x200?addlayer={%22id%22:%22general%22,%22type%22:%22raster%22,%22source-layer%22:%22water%22,%22source%22:{%22type%22:%22raster%22,%22tiles%22:[%22//gibs-a.earthdata.nasa.gov/wmts/epsg3857/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2020-10-03/GoogleMapsCompatible_Level9/{z}/{y}/{x}.jpg%22,%22//gibs-b.earthdata.nasa.gov/wmts/epsg3857/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2020-10-03/GoogleMapsCompatible_Level9/{z}/{y}/{x}.jpg%22,%22//gibs-c.earthdata.nasa.gov/wmts/epsg3857/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2020-10-03/GoogleMapsCompatible_Level9/{z}/{y}/{x}.jpg%22],%22tileSize%22:256},%22minzoom%22:0,%22maxzoom%22:10,%22layout%22:{%22visibility%22:%22visible%22}}&access_token=pk.eyJ1IjoidGVhbWFudHJpa3NoIiwiYSI6ImNrZnQ3dHUxNjBwMjQycXBkNTB6Y2NidXYifQ.bJUa4zjIUDITAZLgc8hz2A";
+    window.open(a);
   };
 
   return (
-    <div style={{ maxHeight: "50vh" }}>
-      <div className="sidebarStyle">
-        <div>
-          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-          <button onClick={getPic}>Pic</button>
+    <>
+      <section style={{ height: "75vh", position: "relative" }}>
+        <div className="sidebarStyle">
+          <div>
+            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            <button onClick={getPic}>Pic</button>
+          </div>
         </div>
-      </div>
-      <div className="map-container" ref={mapContainerRef} />
-    </div>
+        <div className="map-container" ref={mapContainerRef} />
+      </section>
+      {/* <img src={imgsrc} /> */}
+    </>
   );
 };
 
